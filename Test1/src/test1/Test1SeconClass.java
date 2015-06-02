fcffe0cf99924b30beeeee5f176afe47d93ce07a
sailfishqa <shabarishqa@gmail.com>
2015-06-02 14:47:50 +0530

    first code

---
+++ Test1/src/test1/Test1SeconClass.java
@@ -0,0 +1,16 @@
+package test1;
+
+public class Test1SeconClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test1SeconClass");
+		System.out.println("-------------");
+		System.out.println("Hi First Class");
+		System.out.println("Hi First Class");
+		System.out.println("Hi First Class");
+		System.out.println("Hi First Class");
+		System.out.println("Hi First Class");
+	}
+
+}
---
+++ Test1/src/test1/Test1ThirdClass.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class Test1ThirdClass {
+	public static void main(String[] args){
+	System.out.println("Test1ThirdClass");
+	System.out.println("-------------");
+	System.out.println("Hi First Class");
+	System.out.println("Hi First Class");
+	System.out.println("Hi First Class");
+	System.out.println("Hi First Class");
+	System.out.println("Hi First Class");
+}}
